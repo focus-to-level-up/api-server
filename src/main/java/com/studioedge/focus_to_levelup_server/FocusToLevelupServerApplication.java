49f9a87b72cf2ed159dfaa5e458c4859ece2553b
sangyeong01 <tkddud386@gmail.com>
2025-10-19 16:38:04 +0900

    feat: Initialize Spring Boot project with Gradle
    Signed-off-by: sangyeong01 <tkddud386@gmail.com>

---
+++ src/main/java/com/studioedge/focus_to_levelup_server/FocusToLevelupServerApplication.java
@@ -0,0 +1,13 @@
+package com.studioedge.focus_to_levelup_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusToLevelupServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusToLevelupServerApplication.class, args);
+	}
+
+}
