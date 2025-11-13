122235dde005454542de9a84936604bd527422d3
singsangssong <plmko0914@gmail.com>
2025-11-13 16:41:40 +0900

    refactor: refactor all PR comment

--- src/main/java/com/studioedge/focus_to_levelup_server/global/common/AppConstants.java
+++ src/main/java/com/studioedge/focus_to_levelup_server/global/common/AppConstants.java
@@ -11,16 +11,12 @@ public final class AppConstants {
     private AppConstants() {}
 
     /**
-<<<<<<< HEAD
-     * 회원가입 시 지급되는 기본 프로필 에셋의 이름 목록
-=======
      * 회원가입 시 지급되는 기본 캐릭터의 이름입니다.
      */
     public static final String DEFAULT_CHARACTER_NAME = "양동동";
 
     /**
      * 회원가입 시 지급되는 기본 프로필 에셋의 이름 목록입니다.
->>>>>>> origin
      */
     public static final List<String> DEFAULT_ASSET_NAMES = List.of(
             "양동동 1단계 프로필 이미지",
